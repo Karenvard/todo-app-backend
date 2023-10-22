ba8ff51798e30ce50c917c81a8fc7d307e826782
Karen <111676888+Karenvard@users.noreply.github.com>
2023-10-22 16:54:45 +0200

    fixed error on start

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ var express_1 = require("express");
 var cors_1 = require("cors");
 var router_1 = require("./router");
 var app = express_1();
-(0, dotenv_1.config)({});
+dotenv_1.config({});
 app.use(cors_1({}));
 app.use(express_1.json());
 app.use(router_1.router);
